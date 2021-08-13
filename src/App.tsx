61d37890c54b935517f73c215fed23e67b8b10c4
atsuc1973 <atsu1973@gmail.com>
2021-08-13 23:00:56 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import Counter from './Counter';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface AppProps {
+  message?: string;
+}
+const App: React.FunctionComponent<AppProps> = ({message}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <Counter />
+  </div>
   );
 }
+
+App.defaultProps =  {
+  message: 'Hello, defaultProps!'
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
